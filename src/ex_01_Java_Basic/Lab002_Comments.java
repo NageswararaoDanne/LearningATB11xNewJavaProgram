0649f8d4e4d0c9c3c6d79fb6af749ddf964c2464
hp <dannenageswar@gmail.com>
2025-05-28 16:49:05 +0530

    28th May 2025- Comments Class

---
+++ src/ex_01_Java_Basic/Lab002_Comments.java
@@ -0,0 +1,17 @@
+package ex_01_Java_Basic;
+
+public class Lab002_Comments {
+
+    public static void main(String[] args) {
+
+        // Single Line ---> This is a single line comment where compiler will ignored to run.
+
+        // Multi line comments
+        /*1234499898
+        88iowjhdjh
+        ,jbdhiuhuihd
+        kjfhhf   */
+
+        System.out.println("Commented line will be ignored");
+    }
+}
